04141b7d0e9e19803338e9ad1005dae228b81628
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2023-08-25 15:17:12 +0600

    chore: upgrade npm packages

--- src/services/file-storage/local-file-storage.test.ts
+++ src/services/file-storage/local-file-storage.test.ts
@@ -65,7 +65,7 @@ describe('LocalFileStorage', () => {
         RootLocation,
         undefined,
         undefined,
-        () => ({} as unknown as ReadStream)
+        () => ({}) as unknown as ReadStream
       );
 
       res = await storage.load(Filename);
